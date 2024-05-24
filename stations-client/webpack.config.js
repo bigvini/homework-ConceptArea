5dc779a0de7f03d4b422323b097b88d1991f6024
Vershynin Kostia <bigvini@gmail.com>
2024-05-24 14:25:25 +0300

    add bd

--- stations-client/webpack.config.js
+++ stations-client/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, 'src', 'script.js'), // Виправте 'scr' на 'src'
+    entry: path.resolve(__dirname, 'src', 'script.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
